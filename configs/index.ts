4b3accc75197dc2f41f38dc768886853fff62db8
Louis Ramos <louisramosdev@gmail.com>
2024-04-02 23:37:33 +0200

    add nuxt config for nuxt specific components casing

--- configs/index.ts
+++ configs/index.ts
@@ -1,2 +1,3 @@
+export { default as nuxt } from "./nuxt.js"
 export { default as typescript } from "./typescript.js"
 export { default as vue } from "./vue.js"
---
+++ configs/nuxt.ts
@@ -0,0 +1,34 @@
+import type { Linter } from "eslint"
+import vue from "./vue.js"
+
+const config: Linter.FlatConfig[] = [
+  ...vue,
+  {
+    files: ["**/*.vue"],
+    rules: {
+      "vue/component-name-in-template-casing": ["error", "PascalCase", {
+        globals: [
+          "ClientOnly",
+          "DevOnly",
+          "KeepAlive",
+          "NuxtErrorBoundary",
+          "NuxtImg",
+          "NuxtIsland",
+          "NuxtLayout",
+          "NuxtLink",
+          "NuxtLoadingIndicator",
+          "NuxtPage",
+          "NuxtPicture",
+          "NuxtWelcome",
+          "Suspense",
+          "Teleport",
+          "Transition",
+          "TransitionGroup"
+        ],
+        registeredComponentsOnly: false
+      }]
+    }
+  }
+]
+
+export default config
